7520394421a19c3dcbe8fd9fab6a775562445b59
Vlad Polyakov <s9693118@gmail.com>
2024-04-30 08:14:40 +0300

    add admin page for restaurant

---
+++ src/assets/assets.js
@@ -0,0 +1,21 @@
+import logo from './logo.png'
+import add_icon from './add_icon.png'
+import order_icon from './order_icon.png'
+import profile_image from './profile_image.png'
+import upload_area from './upload_area.png'
+import parcel_icon from './parcel_icon.png'
+import profile_admin_image from './profile_admin_image.jpg'
+import profile_admin_image_edit from './profile_admin_image_edit.jpg'
+
+export const assets ={
+    logo,
+    add_icon,
+    order_icon,
+    profile_image,
+    profile_admin_image,
+    profile_admin_image_edit,
+    upload_area,
+    parcel_icon
+}
+
+export const url = 'http://localhost:4000'
